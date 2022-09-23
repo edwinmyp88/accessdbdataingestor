b2178e4112a47032108c2713f6dbe6f22bcb0056
Han Wei Tan <hwtan@macrovention.com>
2022-09-23 16:50:25 +0800

    Add project files.

---
+++ MachineDatabase/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mv-ac\source\repos\MachineDatabase\MachineDatabase\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
